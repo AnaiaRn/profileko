9d0b21ce65255925a0cf9303cd606ba69b778415
Anaia-Randrianantenaina <anaiarandrianantenaina@gmail.com>
2025-05-09 17:50:48 +0300

    navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,37 @@
+import { SiGmail } from "react-icons/si"; 
+import { AiFillInstagram } from "react-icons/ai"; 
+import { AiFillGithub } from "react-icons/ai"; 
+import { AiFillLinkedin } from "react-icons/ai"; 
+import logo from '../assets/5.png'
+
+const Navbar = () => {
+  return (
+   <nav className="flex items-center justify-between py-6">
+    <div className="flex flex-shrink-0 items-center">
+        <a href="/" aria-label="Home">
+          <img src={logo} alt="logo" className='mx-2' width={70} height={33}/>  
+        </a>
+    </div>
+    <div className='m-8 flex items-center justify-center gap-4 text-2xl'>
+        <a href="https://www.linkdine.com/in/Anaïa+Randrianantenaina"
+        target='_blank' rel='noopener noreferrer' aria-label='LinkedIn'>
+            <AiFillLinkedin />
+        </a>
+        <a href="https://www.github.com/in/AnaiaRn"
+        target='_blank' rel='noopener noreferrer' aria-label='GitHub'>
+            <AiFillGithub />
+        </a>
+        <a href="https://www.instagram.com/in/anaia002"
+        target='_blank' rel='noopener noreferrer' aria-label='Instagram'>
+            <AiFillInstagram />
+        </a>
+        <a href="https://www.gmail.com/in/anaiarandrianantenaina@gmail.com"
+        target='_blank' rel='noopener noreferrer' aria-label='Gmail'>
+            <SiGmail />
+        </a>
+    </div>
+   </nav>
+  )
+}
+
+export default Navbar

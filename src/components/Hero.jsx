616b0c96af4a50c9f13a370006a32528541946eb
Anaia-Randrianantenaina <anaiarandrianantenaina@gmail.com>
2025-05-10 10:18:57 +0300

    frame motion Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -31,29 +31,29 @@ const Hero = () => {
         <div className="w-full lg:w-1/2">
             <motion.div className="flex flex-col items-center lg:items-start mt-10"
             initial="hidden" animate="visible" variants={containerVariants} >
-                <h2 className="pb-2 text-4xl tracking-tighter lg:text-8xl">
+                <motion.h2 className="pb-2 text-4xl tracking-tighter lg:text-8xl" variants={childVariants}>
                     Anaïa Randrianantenaina
-                </h2>
-                <span className="bg-gradient-to-r from-stone-300 to-stone-600
-                 bg-clip-text text-3xl tracking-tight">
+                </motion.h2>
+                <motion.span className="bg-gradient-to-r from-stone-300 to-stone-600
+                 bg-clip-text text-3xl tracking-tight" variants={childVariants}>
                     Frontend Developer / UI/UX Designer / Wordpress Developer
-                 </span>
-                 <p className="my-2 max-w-lg py-6 text-xl leading-relaxed tracking-tighter">
+                 </motion.span>
+                 <motion.p className="my-2 max-w-lg py-6 text-xl leading-relaxed tracking-tighter">
                   Hello! I'm a passionate frontend developer, specialized in React and Next.js.
-I love building modern, dynamic, and high-performance interfaces that bring ideas to life.
+                  I love building modern, dynamic, and high-performance interfaces that bring ideas to life.
 
-With a strong eye for UI/UX design, I craft user experiences that are both intuitive and visually appealing.
-I pay attention to every detail to ensure smooth and responsive user journeys across all screen sizes.
+                  With a strong eye for UI/UX design, I craft user experiences that are both intuitive and visually appealing.
+                  I pay attention to every detail to ensure smooth and responsive user journeys across all screen sizes.
 
- I also create professional and customized websites using WordPress, combining flexibility, speed, and tailored solutions to meet client needs.
+                  I also create professional and customized websites using WordPress, combining flexibility, speed, and tailored solutions to meet client needs.
 
- My goal? To turn your vision into clean, creative, and functional web solutions.
+                  My goal? To turn your vision into clean, creative, and functional web solutions.
 
-                 </p>
-                 <a  href="../assets/Logo A.R design_fichiers/CV.pdf" target="_blank" rel="noopener noreferrer" download className="bg-white
-                 rounded-full p-4 text-sm text-stone-800 mb-10">
+                 </motion.p>
+                 <motion.a href="../assets/Logo A.R design_fichiers/CV.pdf" target="_blank" rel="noopener noreferrer" download className="bg-white
+                 rounded-full p-4 text-sm text-stone-800 mb-10" variants={childVariants}>
                     Download CV
-                 </a>
+                 </motion.a>
             </motion.div>
         </div>
       </div>
